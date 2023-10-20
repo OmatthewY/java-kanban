0fddd511372e54ee2c270a235e216643b5e3c4e9
Матвей <matvej.oganezov@yandex.ru>
2023-10-20 19:02:46 +0300

    feat: добавлен основной класс Task

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private TaskType type;
+    private TaskStatus status;
+
+    public Task(int id, String name, String description, TaskType type, TaskStatus status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskType getType() {
+        return type;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
 }
