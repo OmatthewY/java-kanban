4ab88d24ee2156b6097002542026df4f8959f064
Матвей <matvej.oganezov@yandex.ru>
2023-10-20 19:01:18 +0300

    feat: добавлен класс Subtask, расширяющий класс Task, и содержащий дополнительно ID эпика

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Subtask {
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(int id, String name, String description, int epicId) {
+        super(id, name, description, TaskType.SUBTASK, TaskStatus.NEW);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
 }
