a5b471a1ffa0183eef761d0110d3d0c30cdbe57f
Матвей <matvej.oganezov@yandex.ru>
2023-11-01 18:52:13 +0300

    feat: добавить команду для вывода истории просмотров

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
+        TaskManager taskManager = new InMemoryTaskManager();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -27,38 +27,25 @@ public class Main {
                 int taskTypeCommand = scanner.nextInt();
                 scanner.nextLine();
 
-                TaskType taskType;
-
                 if (taskTypeCommand == 1) {
-                    taskType = TaskType.EPIC;
+                    Task epic = taskManager.createEpic(name, description);
+                    System.out.println("Создан новый Epic с ID: " + epic.getId());
                 } else if (taskTypeCommand == 2) {
                     System.out.print("Введите ID эпика, к которому относится подзадача: ");
                     int epicId = scanner.nextInt();
                     scanner.nextLine();
 
-                    Task epic = taskManager.findTaskById(epicId);
+                    Task subtask = taskManager.createSubtask(name, description, epicId);
 
-                    if (epic != null && epic.getType() == TaskType.EPIC) {
-                        Task subtask = taskManager.createTask(name, description, TaskType.SUBTASK, epicId);
-
-                        if (subtask != null) {
-                            System.out.println("Создана новая подзадача с ID: " + subtask.getId());
-                        }
-                    } else {
-                        System.out.println("Эпик с указанным ID не найден. Пожалуйста, создайте эпик сначала.");
+                    if (subtask != null) {
+                        System.out.println("Создана новая подзадача с ID: " + subtask.getId());
                     }
-                    continue;
                 } else if (taskTypeCommand == 3) {
-                    taskType = TaskType.NORMAL;
+                    Task normalTask = taskManager.createNormalTask(name, description);
+                    System.out.println("Создана новая Normal задача с ID: " + normalTask.getId());
                 } else {
                     System.out.println("Такого типа не существует. Выберите 1, 2 или 3.");
-                    continue;
                 }
-
-                Task task = taskManager.createTask(name, description, taskType, 0);
-
-                System.out.println("Создана новая задача с ID: " + task.getId());
-
             } else if (command == 2) {
                 System.out.print("Введите ID задачи для обновления: ");
                 int taskId = scanner.nextInt();
@@ -73,10 +60,26 @@ public class Main {
                     System.out.print("Введите новое описание задачи: ");
                     String updatedDescription = scanner.nextLine();
 
-                    System.out.print("Выберите новый статус задачи (NEW, IN_PROGRESS, DONE): ");
-                    TaskStatus updatedStatus = TaskStatus.valueOf(scanner.nextLine().toUpperCase());
-                    taskManager.updateTask(taskId, updatedName, updatedDescription, updatedStatus);
+                    System.out.print("Введите новый статус задачи (NEW, IN_PROGRESS, DONE): ");
+                    String statusInput = scanner.nextLine();
 
+                    TaskStatus updatedStatus = null;
+
+                    try {
+                        updatedStatus = TaskStatus.valueOf(statusInput.toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Недопустимый статус задачи. Пожалуйста," +
+                                " введите NEW, IN_PROGRESS или DONE.");
+                        continue;
+                    }
+
+                    if (taskToUpdate.getType() == TaskType.EPIC) {
+                        taskManager.updateEpic(taskId, updatedName, updatedDescription, updatedStatus);
+                    } else if (taskToUpdate.getType() == TaskType.SUBTASK) {
+                        taskManager.updateSubtask(taskId, updatedName, updatedDescription, updatedStatus);
+                    } else if (taskToUpdate.getType() == TaskType.NORMAL) {
+                        taskManager.updateNormalTask(taskId, updatedName, updatedDescription, updatedStatus);
+                    }
                     System.out.println("Задача успешно обновлена.");
                 } else {
                     System.out.println("Задача с указанным ID не найдена.");
@@ -89,23 +92,35 @@ public class Main {
                 Task taskToDelete = taskManager.findTaskById(taskIdToDelete);
 
                 if (taskToDelete != null) {
-                    taskManager.deleteTask(taskIdToDelete);
-
+                    if (taskToDelete.getType() == TaskType.EPIC) {
+                        taskManager.deleteEpic(taskIdToDelete);
+                    } else if (taskToDelete.getType() == TaskType.SUBTASK) {
+                        taskManager.deleteSubtask(taskIdToDelete);
+                    } else if (taskToDelete.getType() == TaskType.NORMAL) {
+                        taskManager.deleteNormalTask(taskIdToDelete);
+                    }
                     System.out.println("Задача успешно удалена.");
                 } else {
                     System.out.println("Задача с указанным ID не найдена.");
                 }
             } else if (command == 4) {
-                List<Task> allTasks = taskManager.getAllTasks();
+                List<Task> allEpics = taskManager.getAllEpics();
+                List<Task> allSubtasks = taskManager.getAllSubtasks();
+                List<Task> allNormalTasks = taskManager.getAllNormalTasks();
 
-                if (allTasks.isEmpty()) {
-                    System.out.println("Список задач пуст.");
-                } else {
-                    System.out.println("Список всех задач:");
+                System.out.println("Список всех Epic задач:");
+                for (Task epic : allEpics) {
+                    System.out.println(epic);
+                }
 
-                    for (Task t : allTasks) {
-                        System.out.println(t);
-                    }
+                System.out.println("Список всех Subtask задач:");
+                for (Task subtask : allSubtasks) {
+                    System.out.println(subtask);
+                }
+
+                System.out.println("Список всех Normal задач:");
+                for (Task normalTask : allNormalTasks) {
+                    System.out.println(normalTask);
                 }
             } else if (command == 5) {
                 System.out.print("Введите ID эпика, для которого вы хотите получить подзадачи: ");
@@ -118,9 +133,20 @@ public class Main {
                     System.out.println("Подзадачи для выбранного эпика отсутствуют.");
                 } else {
                     System.out.println("Список подзадач для выбранного эпика:");
-
                     for (Task subtask : subtasksForEpic) {
                         System.out.println(subtask);
+                    }
+                }
+            } else if (command == 6) {
+                List<Task> history = taskManager.getHistory();
+
+                if (history.isEmpty()) {
+                    System.out.println("История просмотров пуста.");
+                } else {
+                    System.out.println("История просмотров:");
+
+                    for (Task task : history) {
+                        System.out.println(task);
                     }
                 }
             } else if (command == 0) {
@@ -139,6 +165,7 @@ public class Main {
         System.out.println("3. Удалить задачу");
         System.out.println("4. Показать все задачи");
         System.out.println("5. Показать подзадачи");
+        System.out.println("6. Показать историю просмотров");
         System.out.println("0. Выход");
         System.out.print("Выберите действие: ");
     }
