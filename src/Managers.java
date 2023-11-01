abd971044df73d7a9b260480188ace4251d549f1
Матвей <matvej.oganezov@yandex.ru>
2023-11-01 18:53:37 +0300

    feat: добавить утилитарный класс

--- src/Managers.java
+++ src/Managers.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Managers {
+public class Managers {
+    private static TaskManager taskManager = new InMemoryTaskManager();
+    private static HistoryManager historyManager = new InMemoryHistoryManager();
+
+    public static TaskManager getDefault() {
+        return taskManager;
+    }
+
+    public static HistoryManager getDefaultHistory() {
+        return historyManager;
+    }
 }
