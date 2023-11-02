4f80935285bdb9034a775e5afe18adfb7a384175
Матвей <matvej.oganezov@yandex.ru>
2023-11-02 16:19:33 +0300

    fix: исправить хранение задач

--- src/InMemoryTaskManager.java
+++ src/InMemoryTaskManager.java
@@ -5,32 +5,35 @@ import tasks.models.Task;
 import tasks.models.Subtask;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryTaskManager implements TaskManager {
     private int nextId = 1;
-    private List<Task> tasks = new ArrayList<>();
+    private Map<Integer, Task> epicsMap = new HashMap<>();
+    private Map<Integer, Task> subtasksMap = new HashMap<>();
+    private Map<Integer, Task> normalTasksMap = new HashMap<>();
     private static HistoryManager historyManager = Managers.getDefaultHistory();
 
     @Override
     public Task createEpic(String name, String description) {
         int taskId = nextId++;
         Task epic = new Epic(taskId, name, description, TaskStatus.NEW);
-        tasks.add(epic);
-
+        epicsMap.put(taskId, epic);
 
         return epic;
     }
 
     @Override
     public Task createSubtask(String name, String description, int epicId) {
-        Task epic = findTaskById(epicId);
+        Task epic = epicsMap.get(epicId);
 
         if (epic != null && epic.getType() == TaskType.EPIC) {
             int taskId = nextId++;
 
             Task subtask = new Subtask(taskId, name, description, epicId);
-            tasks.add(subtask);
+            subtasksMap.put(taskId, subtask);
 
 
             ((Epic) epic).addSubtaskId(taskId);
@@ -46,8 +49,7 @@ public class InMemoryTaskManager implements TaskManager {
         int taskId = nextId++;
 
         Task normalTask = new Task(taskId, name, description, TaskType.NORMAL, TaskStatus.NEW);
-        tasks.add(normalTask);
-
+        normalTasksMap.put(taskId, normalTask);
 
         return normalTask;
     }
@@ -106,20 +108,17 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public void deleteEpic(int id) {
-        Task task = findTaskById(id);
+        Task epic = epicsMap.get(id);
 
-        if (task != null && task.getType() == TaskType.EPIC) {
-            tasks.remove(task);
+        if (epic != null) {
+            epicsMap.remove(id);
 
-
-            List<Integer> subtaskIds = ((Epic) task).getSubtaskIds();
+            List<Integer> subtaskIds = ((Epic) epic).getSubtaskIds();
 
             for (int subtaskId : subtaskIds) {
-                Task subtask = findTaskById(subtaskId);
+                Task subtask = subtasksMap.get(subtaskId);
                 if (subtask != null) {
-                    tasks.remove(subtask);
-
-
+                    subtasksMap.remove(subtaskId);
                 }
             }
         } else {
@@ -129,12 +128,10 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public void deleteSubtask(int id) {
-        Task task = findTaskById(id);
+        Task subtask = subtasksMap.get(id);
 
-        if (task != null && task.getType() == TaskType.SUBTASK) {
-            tasks.remove(task);
-
-
+        if (subtask != null) {
+            subtasksMap.remove(id);
         } else {
             System.out.println("Подзадача с указанным ID не найдена.");
         }
@@ -142,12 +139,10 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public void deleteNormalTask(int id) {
-        Task task = findTaskById(id);
+        Task normalTask = normalTasksMap.get(id);
 
-        if (task != null && task.getType() == TaskType.NORMAL) {
-            tasks.remove(task);
-
-
+        if (normalTask != null) {
+            normalTasksMap.remove(id);
         } else {
             System.out.println("Задача с указанным ID не найдена.");
         }
@@ -155,50 +150,46 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public Task findTaskById(int id) {
-        for (Task task : tasks) {
-            if (task.getId() == id) {
-                return task;
-            }
+        Task task = epicsMap.get(id);
+        if (task == null) {
+            task = subtasksMap.get(id);
         }
-        System.out.println("Задача с указанным ID не найдена.");
-        return null;
+        if (task == null) {
+            task = normalTasksMap.get(id);
+        }
+
+        if (task == null) {
+            System.out.println("Задача с указанным ID не найдена.");
+        }
+        return task;
     }
 
     @Override
     public List<Task> getAllEpics() {
-        List<Task> epics = new ArrayList<>();
+        List<Task> epics = new ArrayList<>(epicsMap.values());
 
-        for (Task task : tasks) {
-            if (task.getType() == TaskType.EPIC) {
-                epics.add(task);
-                updateViewedTasks(task);
-            }
+        for (Task epic : epics) {
+            updateViewedTasks(epic);
         }
         return epics;
     }
 
     @Override
     public List<Task> getAllSubtasks() {
-        List<Task> subtasks = new ArrayList<>();
+        List<Task> subtasks = new ArrayList<>(subtasksMap.values());
 
-        for (Task task : tasks) {
-            if (task.getType() == TaskType.SUBTASK) {
-                subtasks.add(task);
-                updateViewedTasks(task);
-            }
+        for (Task subtask : subtasks) {
+            updateViewedTasks(subtask);
         }
         return subtasks;
     }
 
     @Override
     public List<Task> getAllNormalTasks() {
-        List<Task> normalTasks = new ArrayList<>();
+        List<Task> normalTasks = new ArrayList<>(normalTasksMap.values());
 
-        for (Task task : tasks) {
-            if (task.getType() == TaskType.NORMAL) {
-                normalTasks.add(task);
-                updateViewedTasks(task);
-            }
+        for (Task normalTask : normalTasks) {
+            updateViewedTasks(normalTask);
         }
         return normalTasks;
     }
@@ -207,7 +198,7 @@ public class InMemoryTaskManager implements TaskManager {
     public List<Task> getSubtasksForEpic(int epicId) {
         List<Task> subtasks = new ArrayList<>();
 
-        for (Task task : tasks) {
+        for (Task task : subtasksMap.values()) {
             if (task.getType() == TaskType.SUBTASK && ((Subtask) task).getEpicId() == epicId) {
                 subtasks.add(task);
             }
@@ -215,7 +206,7 @@ public class InMemoryTaskManager implements TaskManager {
         return subtasks;
     }
 
-    private void updateViewedTasks(Task task) {
+    public void updateViewedTasks(Task task) {
 
         if (!historyManager.getHistory().contains(task)) {
             historyManager.add(task);
